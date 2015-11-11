78e72542e0492846a2ddc2b54b9593f9f9449af8
Hao <orctom@gmail.com>
2015-11-12 01:41:10 +0800

    execute the script

--- src/main/groovy/com/orctom/gradle/was/WebSphereServiceImpl.groovy
+++ src/main/groovy/com/orctom/gradle/was/WebSphereServiceImpl.groovy
@@ -4,12 +4,14 @@ import com.orctom.was.model.Command
 import com.orctom.was.model.WebSphereModel
 import com.orctom.was.service.impl.AbstractWebSphereServiceImpl
 import org.gradle.api.Project
+import org.gradle.process.ExecResult
+import org.gradle.process.ExecSpec
 
 /**
  * Deploy to was
  * Created by hao on 11/11/15.
  */
-class WebSphereServiceImpl extends AbstractWebSphereServiceImpl{
+class WebSphereServiceImpl extends AbstractWebSphereServiceImpl {
 
     private Project project
 
@@ -20,8 +22,16 @@ class WebSphereServiceImpl extends AbstractWebSphereServiceImpl{
 
     @Override
     protected void executeCommand(Command command) {
-        project.exec {
-            commandLine = ['echo', 'hello world']
+        ExecResult result = project.exec { ExecSpec exec ->
+            exec.executable = command.executable
+            exec.workingDir = command.workingDir
+            exec.args = args = command.argsAsList
+            exec.standardInput = System.in
+            exec.standardOutput = System.out
+            exec.errorOutput = System.out
+        }
+        if (result.exitValue != 0) {
+            new RuntimeException("Failed to execute, recturn code ${exitVal}")
         }
     }
 }
